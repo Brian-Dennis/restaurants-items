9ebeebccc571c75e3686b12ce042f62ca47a7882
Brian-Dennis <briandennis49@outlook.com>
2017-09-12 17:03:32 -0700

    adding user model

--- database_setup.py
+++ database_setup.py
@@ -1,19 +1,18 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
 
-class Users(Base):
-    __tablename__ = 'users'
+class User(Base):
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    username = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
-    password = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 
 class Restaurant(Base):
@@ -21,6 +20,8 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -41,6 +42,8 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -54,5 +57,7 @@ class MenuItem(Base):
         }
 
 
-engine = create_engine('mysql://root@localhost/restaurantmenu')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+
+
 Base.metadata.create_all(engine)
